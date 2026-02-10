bd16f648e3660a071b9cd70ae03adef03792c459
Raissa12best <bestraissa20@gmail.com>
2026-02-10 10:57:33 +0200

    react content

---
+++ react-one/src/components/reactFacts.jsx
@@ -0,0 +1,18 @@
+function ReactFacts(){
+    return(
+        <div>
+            <img src="/src/assets/react.svg"/> 
+            <h1>Fun facts about React</h1>  
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was orginally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Meta</li>
+            </ul>
+            <footer>
+                <p>2026 built with love by a dev</p>
+            </footer>
+                 </div>
+    )
+}
+export default ReactFacts;
---
+++ react-one/src/components/reactOne.jsx
@@ -0,0 +1,15 @@
+function Message(){
+    return(
+        <body>
+    <h1 className="header">Welcome , Bienvenue, Buenos Dias</h1>
+      <h2>We can start now!</h2>
+      </body>
+    )
+}
+
+export  default Message;
+
+// function paragraph(){
+//     return<h2>We can start now!</h2>
+// }
+// export  paragraph;
